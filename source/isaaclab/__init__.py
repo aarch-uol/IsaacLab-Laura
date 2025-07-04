cc83e275c062fb1daaaf0933e8c73f56c5246d56
Harry <harrywfcheung@gmail.com>
2025-07-04 17:51:16 +0100

    fixed app launcher point error, readded tests (removed frmo ignore)

--- source/isaaclab/__init__.py
+++ source/isaaclab/__init__.py
@@ -12,7 +12,8 @@ import toml
 ISAACLAB_EXT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 """Path to the extension source directory."""
 
-ISAACLAB_METADATA = toml.load(os.path.join(ISAACLAB_EXT_DIR, "config", "extension.toml"))
+# ISAACLAB_METADATA = toml.load(os.path.join(ISAACLAB_EXT_DIR, "config", "extension.toml"))
+ISAACLAB_METADATA = toml.load(os.path.join(os.path.dirname(__file__), "extension.toml"))
 """Extension metadata dictionary parsed from the extension.toml file."""
 
 # Configure the module-level variables
